6d6d538b14b799f56568e5f38bc743df09cb1bc7
Rafael dos Santos <p.4ever@gmail.com>
2018-04-21 13:34:45 -0300

    Mvc sample

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gwab2018.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
